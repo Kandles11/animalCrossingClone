2f7e37c34331ef0f2d9a27ac71ca6396be5d9ca2
kandles11 <kandleseatwicks@gmail.com>
2020-04-16 13:59:32 -0500

    intial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("mainCanvas");
+var context = canvas.getContext("2d");
+
+canvas.height=window.innerWidth;
+canvas.width=window.innerHeight;
+
+var characterImg = new Image();
+characterImg.onload = function() {
+  context.drawImage(characterImg, 20, 20, 100, 100)
+}
+characterImg.src = 'assets/char.png'
